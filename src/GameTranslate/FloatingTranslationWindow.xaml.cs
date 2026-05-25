779b080704d2efd723d0932f8abdb8784b9bd1fe
xueyikang <xueyikang@xueyikangdeMac-mini.local>
2026-05-25 10:22:08 +0800

    test scroll floating result to bottom

--- src/GameTranslate/FloatingTranslationWindow.xaml.cs
+++ src/GameTranslate/FloatingTranslationWindow.xaml.cs
@@ -1,7 +1,9 @@
+using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls.Primitives;
 using System.Windows.Input;
 using System.Windows.Media;
+using GameTranslate.ViewModels;
 using WpfScrollBar = System.Windows.Controls.Primitives.ScrollBar;
 
 namespace GameTranslate;
@@ -11,6 +13,44 @@ public partial class FloatingTranslationWindow : Window
     public FloatingTranslationWindow()
     {
         InitializeComponent();
+        DataContextChanged += FloatingTranslationWindow_DataContextChanged;
+    }
+
+    protected override void OnClosed(EventArgs e)
+    {
+        UnsubscribeFromViewModel(DataContext);
+        base.OnClosed(e);
+    }
+
+    private void FloatingTranslationWindow_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+    {
+        UnsubscribeFromViewModel(e.OldValue);
+        SubscribeToViewModel(e.NewValue);
+        Dispatcher.BeginInvoke(() => FloatingResultScrollViewer.ScrollToEnd());
+    }
+
+    private void SubscribeToViewModel(object? dataContext)
+    {
+        if (dataContext is MainViewModel viewModel)
+        {
+            viewModel.PropertyChanged += ViewModel_PropertyChanged;
+        }
+    }
+
+    private void UnsubscribeFromViewModel(object? dataContext)
+    {
+        if (dataContext is MainViewModel viewModel)
+        {
+            viewModel.PropertyChanged -= ViewModel_PropertyChanged;
+        }
+    }
+
+    private void ViewModel_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(MainViewModel.TranslatedText))
+        {
+            Dispatcher.BeginInvoke(() => FloatingResultScrollViewer.ScrollToEnd());
+        }
     }
 
     private void Window_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
